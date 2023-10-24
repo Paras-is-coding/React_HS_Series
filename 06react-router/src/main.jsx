c1bec762723f7168e6968cb3efdb6e5ab8f45728
Paras-is-coding <paraschand5815@gmail.com>
2023-10-24 20:58:19 +0545

    another way to add route paths inside main.jsx

--- 06react-router/src/main.jsx
+++ 06react-router/src/main.jsx
@@ -2,23 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Home from './components/home/Home.jsx'
 import Contact from './components/contact/Contact.jsx'
 import About from './components/about/About.jsx'
+import { Route } from 'react-router-dom'
+// const router = createBrowserRouter(
+//   [
+//     {
+//       path:"/",
+//       element:<App/>,
+//       children:[
+//         {path:"",element:<Home/>},
+//         {path:"about",element:<About/>},
+//         {path:"contact",element:<Contact/>}
+//       ]
+//     }
+//   ]
+// )
 
-const router = createBrowserRouter(
-  [
-    {
-      path:"/",
-      element:<App/>,
-      children:[
-        {path:"",element:<Home/>},
-        {path:"about",element:<About/>},
-        {path:"contact",element:<Contact/>}
-      ]
-    }
-  ]
+// SYNTAX II 
+
+const router = createBrowserRouter( 
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='' element={<Home/>} />
+      <Route path='about' element={<About/>} />
+      <Route path='contact' element={<Contact/>} />
+    </Route>
+  )
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
