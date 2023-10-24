6c765bafecbcf7336e1d4a9a6f40dd5895b533bc
Paras-is-coding <paraschand5815@gmail.com>
2023-10-24 20:51:34 +0545

    used react-router-dom for creating routing syntaxI

--- 06react-router/src/main.jsx
+++ 06react-router/src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/home/Home.jsx'
+import Contact from './components/contact/Contact.jsx'
+import About from './components/about/About.jsx'
+
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:<App/>,
+      children:[
+        {path:"",element:<Home/>},
+        {path:"about",element:<About/>},
+        {path:"contact",element:<Contact/>}
+      ]
+    }
+  ]
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
